4a7e01e171526cd6a50dce44e1fb84b8f46f2f0c
KrisKonov <61319367+KrisKonov@users.noreply.github.com>
2021-01-22 09:27:41 +0200

    Update Money.cs

--- salaries/Money.cs
+++ salaries/Money.cs
@@ -6,11 +6,24 @@ namespace salaries
     class Money
     {
 
+
+       public double Doo { get; set; }
+        public double dzpo { get; set; }
+
+        public double zdravno { get; set; }
+
+        public double ddfl { get; set; }
+
+
         public double Salary { get; set; }
 
         public int Experience { get; set; }
 
         public double Bonus { get; set; }
+
+        public double taxes{ get; set; }
+
+        
 
         public void GetExp()
         {
@@ -21,18 +34,27 @@ namespace salaries
             this.Bonus = double.Parse(Console.ReadLine());
             Console.Write("What is the experience of the worker?");
             this.Experience = int.Parse(Console.ReadLine());
+            Doo = Salary * 8.38 / 100;
+            dzpo = Salary * 2.20 / 100;
+            zdravno = Salary * 3.20 / 100;
+            taxes = taxes + Doo + dzpo + zdravno;
+            Salary = Salary - taxes;
+            ddfl = Salary * 10 / 100;
 
-            if (Experience <= 20)
+            for (int i = 1; i <= Experience; i++)
             {
-                for (int i = 1; i <= Experience; i++)
-                {
-                    Console.WriteLine(Salary);
-
-                    Salary = Salary + Salary * Bonus / 100;
-                }
-                Console.WriteLine("You have earned {0} for this period of time", Salary * Experience);
+                
+                Console.WriteLine(Salary - ddfl);
+                Salary = Salary + Salary * Bonus / 100;
 
             }
+
+        
+
+         
+
+
+            
         }
 
 
